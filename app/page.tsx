5a5841fb2c446f0629898ce99dc261bc9282ae71
Héctor Muñoz <hectormme@ext.inditex.com>
2025-05-03 18:14:18 +0200

    First steps

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import { Navbar1 } from "@/components/navbar1";
 
 export default function Home() {
   return (
+  
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Navbar1 />
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -97,6 +101,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
+        <Button>Click me</Button>
       </footer>
     </div>
   );
